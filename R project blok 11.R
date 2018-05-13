69a139d501cb3b66b137696d1a7043c5ceb07f6e
SmitsG <smitsg@ziggo.nl>
2018-05-13 15:28:40 +0200

    Starting from count table, Data exploration and quality assesment

--- R project blok 11.R
+++ R project blok 11.R
@@ -1,5 +1,11 @@
 # install.packages("rstudioapi") # run this if it's your first time using it to install
-
+# source("https://bioconductor.org/biocLite.R") install bioconductor if not already installed
+# library(edgeR) # for DGEList
+# library(limma)
+# library(RColorBrewer)
+# library(mixOmics)
+biocLite("HTSFilter") # 
+#############create a function set_wd that sets the working directory############## 
 set_wd <- function() {
   # load rstudioapi # make sure you have it installed
   library(rstudioapi)
@@ -11,11 +17,63 @@ set_wd <- function() {
   print( getwd() )
 }
 
+# execute set_wd
 set_wd()
 
-# Get files
+################################preprocessing data#######################################
+#Importing the files
 # data <- read.delim(file.choose(), header=T)
 WCFS1_anno <- read.delim("WCFS1_anno.txt", header=T)
 RNA_Seq_counts <- read.delim("RNA_Seq_counts.txt", header=T)
 
-merged_data = merge(RNA_Seq_counts, WCFS1_anno, by.x="ID", by.y="ORF")
+# Have a look at the count data:
+# head(RNA_Seq_counts) 
+# nrow(RNA_Seq_counts)  
+
+# merge the data
+merged_data = merge(merged_data_Seq_counts, WCFS1_anno, by.x="ID", by.y="ORF")
+
+# remove NA values
+merged_data <- merged_data[1:(length(merged_data)-4)]
+
+#############################Starting from count table##############################
+# create DGEList
+exp <- c("WCFS1.glc","WCFS1.glc","WCFS1.rib","WCFS1.rib")
+y <- DGEList(counts=RNA_Seq_counts[,2:5],group=group)
+
+
+#######################Data exploration and quality assessment######################
+# Extract pseudo-counts
+pseudoCounts <- log2(y$counts+1)
+head(pseudoCounts)
+
+# Create histogram for pseudo-counts 
+hist(pseudoCounts[,"WCFS1.glc.1"])
+hist(pseudoCounts[,"WCFS1.glc.2"])
+hist(pseudoCounts[,"WCFS1.rib.1"])
+hist(pseudoCounts[,"WCFS1.rib.2"])
+
+# Create boxplot for pseudo-counts
+boxplot(pseudoCounts, col="gray", las=3)
+
+# Create MA plot for pseudo-counts
+par(mfrow=c(1,2))
+
+## WT1 vs WT2
+# M values
+M_values <- (pseudoCounts[,1] - pseudoCounts[,2])
+
+# A values
+A_values <- (pseudoCounts[,1] + pseudoCounts[,2])/2
+
+plot(A_values, M_values, xlab="A", ylab="M", pch=19, main="treated")
+abline(h=0, col="red")
+
+## Mt1 vs Mt2
+# M values
+# Nog even kijken of de waardes van M_values / A_values kloppen
+M_values <- (pseudoCounts[,2] - pseudoCounts[,3])
+A_values <- (pseudoCounts[,2] + pseudoCounts[,3])/2
+plot(A_values, M_values, xlab="A", ylab="M", pch=19, main="control")
+abline(h=0, col="red")
+
