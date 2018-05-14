1e7c20d03c4787d71c52e718398d6c57d4c89b25
SmitsG <smitsg@ziggo.nl>
2018-05-14 20:52:49 +0200

    imporoved imports, changed names, added additional calcs, overall improvement

--- R project blok 11.R
+++ R project blok 11.R
@@ -1,10 +1,17 @@
 # install.packages("rstudioapi") # run this if it's your first time using it to install
-# source("https://bioconductor.org/biocLite.R") install bioconductor if not already installed
-# library(edgeR) # for DGEList
-# library(limma)
-# library(RColorBrewer)
-# library(mixOmics)
-# biocLite("HTSFilter") 
+# install.packages("stringi")
+# install.packages(c("RColorBrewer", "mixOmics", "VennDiagram"))
+# install.packages("gplots")
+source("https://bioconductor.org/biocLite.R") # install bioconductor if not already installed
+library(limma)
+library(edgeR) # for DGEList
+library(RColorBrewer)
+library(MASS)
+library(lattice)
+library(ggplot2)
+library(mixOmics)
+library(gplots)
+biocLite("HTSFilter") 
 #############create a function set_wd that sets the working directory############## 
 set_wd <- function() {
   # load rstudioapi # make sure you have it installed
@@ -20,7 +27,7 @@ set_wd <- function() {
 # execute set_wd
 set_wd()
 
-################################preprocessing data#######################################
+################################data#######################################
 #Importing the files
 # data <- read.delim(file.choose(), header=T)
 WCFS1_anno <- read.delim("WCFS1_anno.txt", header=T)
@@ -44,28 +51,28 @@ y <- DGEList(counts=RNA_Seq_counts[,2:5],group=group)
 
 
 #######################Data exploration and quality assessment######################
-# Extract pseudo-counts
-pseudoCounts <- log2(y$counts+1)
-head(pseudoCounts)
+# Extract RNA_Seq_Counts
+RNA_Seq_Counts <- log2(y$counts+1)
+head(RNA_Seq_Counts)
 
 # Create histogram for pseudo-counts 
-hist(pseudoCounts[,"WCFS1.glc.1"])
-hist(pseudoCounts[,"WCFS1.glc.2"])
-hist(pseudoCounts[,"WCFS1.rib.1"])
-hist(pseudoCounts[,"WCFS1.rib.2"])
+hist(RNA_Seq_Counts[,"WCFS1.glc.1"])
+hist(RNA_Seq_Counts[,"WCFS1.glc.2"])
+hist(RNA_Seq_Counts[,"WCFS1.rib.1"])
+hist(RNA_Seq_Counts[,"WCFS1.rib.2"])
 
 # Create boxplot for pseudo-counts
-boxplot(pseudoCounts, col="gray", las=3)
+boxplot(RNA_Seq_Counts, col="gray", las=3)
 
 # Create MA plot for pseudo-counts
 par(mfrow=c(1,2))
 
 ## WT1 vs WT2
 # M values
-M_values <- (pseudoCounts[,1] - pseudoCounts[,2])
+M_values <- (RNA_Seq_Counts[,1] - RNA_Seq_Counts[,2])
 
 # A values
-A_values <- (pseudoCounts[,1] + pseudoCounts[,2])/2
+A_values <- (RNA_Seq_Counts[,1] + RNA_Seq_Counts[,2])/2
 
 plot(A_values, M_values, xlab="A", ylab="M", pch=19, main="treated")
 abline(h=0, col="red")
@@ -73,21 +80,28 @@ abline(h=0, col="red")
 ## Mt1 vs Mt2
 # M values
 # Nog even kijken of de waardes van M_values / A_values kloppen
-M_values <- (pseudoCounts[,2] - pseudoCounts[,3])
-A_values <- (pseudoCounts[,2] + pseudoCounts[,3])/2
+M_values <- (RNA_Seq_Counts[,2] - RNA_Seq_Counts[,3])
+A_values <- (RNA_Seq_Counts[,2] + RNA_Seq_Counts[,3])/2
 plot(A_values, M_values, xlab="A", ylab="M", pch=19, main="control")
 abline(h=0, col="red")
 
 # MDS for pseudo-counts (using limma package)
-plotMDS(pseudoCounts)
+plotMDS(RNA_Seq_Counts)
 
 # heatmap for pseudo-counts (using mixOmics package)
-sampleDists <- as.matrix(dist(t(pseudoCounts)))
-sampleDists
+sampleDists <- as.matrix(dist(t(RNA_Seq_Counts)))
+Color <- colorRampPalette(c("Yellow", "orange")) #set color
+heatmap.2(sampleDists, col=Color, main="Sample",trace="none", margins = c(10,12), cexRow=0.5, cexCol=0.5)
 
 # werkt nog niet nog naar kijken
-# cimColor <- colorRampPalette(rev(brewer.pal(9, "Blues")))(16)
-# cim <- cim(sampleDists, color=cimColor, symkey=FALSE)
+#cimColor <- colorRampPalette(c(brewer.pal(9, "Blues")))(16)
+#par(mar=rep(2,4))
+#heatmap <- cim(sampleDists, color=cimColor, symkey=FALSE)
+#nba.m <- melt(nba)
+#nba.m <- ddply(nba.m, .(variable), transform,
+#               +     rescale = rescale(value))
+
+
 
 #################Differential expression analysis########################
 # remove genes with zero counts for all samples
@@ -101,6 +115,10 @@ head(y$counts)
 
 eff.lib.size <- y$samples$lib.size*y$samples$norm.factors
 normCounts <- cpm(dgeFull)
-pseudoNormCounts <- log2(normCounts + 1)
-boxplot(pseudoNormCounts, col="gray", las=3)
-plotMDS(pseudoNormCounts)
+RNA_Seq_NormCounts <- log2(normCounts + 1)
+boxplot(RNA_Seq_NormCounts, col="gray", las=3)
+plotMDS(RNA_Seq_NormCounts)
+
+# make PCA plot
+# PCA <- prcomp(x.norm, retx=F, center = TRUE, scale = TRUE)
+# text --> text toevoegen aan pca plot
