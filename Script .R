20fe1d8cc0a5b3f4ff12d93faf644b70f9de1d84
Silleke <SCF.vanSambeek@student.han.nl>
2018-05-28 11:29:27 +0200

    Added code to update the annotation. NC8 has a empty merged dataset. Could not find the bug.

---
+++ Script .R
@@ -0,0 +1,16 @@
+library(rtracklayer)
+
+#data = read.delim(file.choose(), sep="\t", skip=1)
+#annotation = read.delim(file.choose(), sep="\t", skip=1)
+#test = merge(data, annotation, by.x = "ID", by.y = "ORF")
+#annotation_updated = import.gff(file.choose())
+
+# Nieuwe versie van het script:
+
+#library(rtracklayer)
+
+data = read.delim(file.choose(), sep="\t", skip=1)
+wcfs1_annotation = import.gff(file.choose())
+nc8_annotation = import.gff(file.choose())
+data_annotated_wcfs1 = merge(data, wcfs1_annotation, by.x = "ID", by.y = "locus_tag")
+data_annotated_nc8 = merge(data, nc8_annotation, by.x = "ID", by.y = "locus_tag")
